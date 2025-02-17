f3eb98b46acf32d31d68cd789ab1215e8d9dc383
Millicentchinelo <millicentchinelo@gmail.com>
2025-02-17 15:30:19 +0100

    from laptop

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! This is deployed using Nodejs');
 });
 
 app.listen(port, () => {
